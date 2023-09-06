b3bd494e9f96c53f6e2457030266cca538641dff
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2023-09-06 14:16:21 +0300

    d

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,39 @@
+import { fetchUser, getActivity } from "@/lib/actions/users.actions";
+import { currentUser } from "@clerk/nextjs";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import {Image} from 'next/image'
+
+const Page = async () => {
+  const user = await currentUser();
+
+  // Clerk will redirect to sign-in page automatically
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const activity = await getActivity(userInfo._id);
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Activity</h1>
+
+
+      <section className="mt-10 flex flex-col gap-5">
+        {
+        
+        activity.length > 0 ? (<>{activity.map((activity)=>(<Link id={activity._id} href={`/thread/${activity.parentId}`}>
+            <article className="activity-class"> <Image src={activity.author.image} alt='Profile Picture' width={20} height={20} className='rounded-full object-cover'></Image></article></Link>))
+            
+            </>): (<p>No activity yet</p>)}
+    </section>
+
+
+
+    </section>
+  );
+};
+
+export default Page;
--- lib/actions/users.actions.ts
+++ lib/actions/users.actions.ts
@@ -5,6 +5,8 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import { addCommentToThread } from "./thread.actions";
 import Thread from "../models/thread.model";
+import { SortOrder } from "mongoose";
+import { FilterQuery } from "mongoose";
 
 interface Params {
   userId: string;
@@ -80,5 +82,79 @@ export async function fetchUserPosts(userId: string) {
     return threads;
   } catch (error: any) {
     console.log(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+interface Props {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: Props) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const regex = new RegExp(searchString, "i");
+
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    };
+
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    const sortOptions = { createdAt: sortBy };
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+    const users = await usersQuery.exec();
+    const isNext = totalUsersCount > skipAmount + users.length;
+    return { users, isNext };
+  } catch (error: any) {
+    console.log(`Failed to fetch users: ${error.message}`);
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // finf all threads created by user
+    const userTheads = await Thread.find({ autor: userId });
+
+    // collect all the child thread ids(replies) from the children
+
+    const childThreadIds = userTheads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "mane image _id",
+    });
+    return replies;
+  } catch (error: any) {
+    console.log(`Failed to fetch activity: ${error.message}`);
   }
 }
